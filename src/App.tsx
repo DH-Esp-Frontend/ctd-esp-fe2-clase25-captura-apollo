ab901cfb3d151f7937c180025f40648456291075
Alejo <alejoivanrojas@gmail.com>
2022-05-16 21:55:04 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle, theme } from './features/styles';
+import { Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { Navbar } from 'features/navigation';
+import { FC } from 'react';
+import HomePage from 'pages/Home.page';
+import LocationPage from 'pages/Location.page';
+import { store } from 'store/store';
+import FollowingPage from 'pages/Following.page';
+import { LanguageComponent, LanguageProvider } from 'features/language';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: FC = () => (
+  <Provider store={store}>
+    <ThemeProvider theme={theme}>
+      <LanguageProvider>
+        <GlobalStyle />
+        <LanguageComponent />
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/location/:id" element={<LocationPage />} />
+          <Route path="/following" element={<FollowingPage />} />
+        </Routes>
+      </LanguageProvider>
+    </ThemeProvider>
+  </Provider>
+);
 
 export default App;
